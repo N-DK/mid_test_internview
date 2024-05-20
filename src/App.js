bd693f8f8d714a0f88ac9ff966f08cf6d2930d37
N_DK <nguyenkhoadang135759@gmail.com>
2024-05-20 11:00:13 +0700

    convertSeconds

--- src/App.js
+++ src/App.js
@@ -5,18 +5,31 @@ import axios from "axios";
 function App() {
   const [data, setData] = useState();
 
+  function convertSeconds(seconds) {
+    // Tính số giờ
+    let hours = Math.floor(seconds / 3600);
+    // Tính số phút còn lại sau khi đã lấy giờ
+    let minutes = Math.floor((seconds % 3600) / 60);
+    // Tính số giây còn lại sau khi đã lấy phút
+    let remainingSeconds = seconds % 60;
+
+    // Trả về kết quả dạng chuỗi
+    return `${hours} giờ ${minutes} phút ${remainingSeconds} giây`;
+  }
+
   useEffect(() => {
     const fetchAPI = async () => {
       const response = await axios.get(
         "https://apisolfive.app.tranviet.site/v2/api/get/playlist/info?id=6IACDA86"
       );
+      console.log(response.data.data.data);
       setData(response.data.data.data);
     };
     fetchAPI();
   }, []);
 
   return (
-    <div style={{background: "#0d1620", color:"#fff"}} className="">
+    <div style={{ background: "#0d1620", color: "#fff" }} className="">
       <div className="p-5">
         <div>
           <div className="d-flex mb-4 align-items-center">
@@ -30,27 +43,36 @@ function App() {
               <p className="mb-4 fs-2 fw-800">{data?.title}</p>
               <div>
                 <p>Danh sách phát • Solfive</p>
-                <p>{data?.song?.total} bài hát • 5 giờ 47 phút 1 giây</p>
-                <p>Nhạc Việt "lâu phai" và gây nghiện hoài hoài</p>
+                <p>{data?.song?.total} bài hát • {convertSeconds(data?.song?.totalDuration)}</p>
+                <p>{data?.sortDescription}</p>
               </div>
             </div>
           </div>
         </div>
         <div>
           {data?.song?.items.map((item, index) => (
-            <div key={index} className="d-flex align-items-center mb-3 border-bottom pb-2">
-              <div style={{justifyContent: 'space-between'}} className="d-flex w-100">
-                <div style={{flex: 1}} className="d-flex align-items-center">
+            <div
+              key={index}
+              className="d-flex align-items-center mb-3 border-bottom pb-2"
+            >
+              <div
+                style={{ justifyContent: "space-between" }}
+                className="d-flex w-100"
+              >
+                <div style={{ flex: 1 }} className="d-flex align-items-center">
                   <div className="rounded overflow-hidden me-4">
                     <img src={item.thumbnail} />
                   </div>
                   <a>{item.title}</a>
                 </div>
-                <div style={{flex: 1}} className="d-flex align-items-center">
-                  <div style={{justifyContent: 'space-between'}} className="d-flex align-items-center w-100">
+                <div style={{ flex: 1 }} className="d-flex align-items-center">
+                  <div
+                    style={{ justifyContent: "space-between" }}
+                    className="d-flex align-items-center w-100"
+                  >
                     <p className="mb-0">
-                      {item.artists.map((artist) => (
-                        <span>{artist.name}, </span>
+                      {item.artists.map((artist, index) => (
+                        <span key={index}>{artist.name}, </span>
                       ))}
                     </p>
                     <p className="mb-0">04:03</p>
