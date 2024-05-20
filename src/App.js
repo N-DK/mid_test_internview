6a7c2a53e9a099d3cd97b291807b15636d4c6fe1
N_DK <nguyenkhoadang135759@gmail.com>
2024-05-20 10:53:36 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      const response = await axios.get(
+        "https://apisolfive.app.tranviet.site/v2/api/get/playlist/info?id=6IACDA86"
+      );
+      setData(response.data.data.data);
+    };
+    fetchAPI();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{background: "#0d1620", color:"#fff"}} className="">
+      <div className="p-5">
+        <div>
+          <div className="d-flex mb-4 align-items-center">
+            <div
+              style={{ width: 200, height: 200 }}
+              className="me-3 rounded overflow-hidden"
+            >
+              <img className="w-100 h-100" src={data?.thumbnail} />
+            </div>
+            <div>
+              <p className="mb-4 fs-2 fw-800">{data?.title}</p>
+              <div>
+                <p>Danh sách phát • Solfive</p>
+                <p>{data?.song?.total} bài hát • 5 giờ 47 phút 1 giây</p>
+                <p>Nhạc Việt "lâu phai" và gây nghiện hoài hoài</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          {data?.song?.items.map((item, index) => (
+            <div key={index} className="d-flex align-items-center mb-3 border-bottom pb-2">
+              <div style={{justifyContent: 'space-between'}} className="d-flex w-100">
+                <div style={{flex: 1}} className="d-flex align-items-center">
+                  <div className="rounded overflow-hidden me-4">
+                    <img src={item.thumbnail} />
+                  </div>
+                  <a>{item.title}</a>
+                </div>
+                <div style={{flex: 1}} className="d-flex align-items-center">
+                  <div style={{justifyContent: 'space-between'}} className="d-flex align-items-center w-100">
+                    <p className="mb-0">
+                      {item.artists.map((artist) => (
+                        <span>{artist.name}, </span>
+                      ))}
+                    </p>
+                    <p className="mb-0">04:03</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
